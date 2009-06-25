f004b5dce5d12932c62fb48f8e74c41b3aa422e3
Mike Douglass <douglm@rpi.edu>
2009-06-25 17:52:40 +0000

    Fix up build so that tz server gets correctly incorporated into the ear. Involved:
      1. Moving the temp expanded war file
      2. changing the name of the ehcache config so it doesn't intefere with th ebedework one.
      3. Fixed Carddav options and bedework options
      4. temp lib was getting built into the ear doubling its size. Removed that

--- tzsvr/src/org/bedework/timezones/server/TzServerUtil.java
+++ tzsvr/src/org/bedework/timezones/server/TzServerUtil.java
@@ -52,6 +52,7 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.StringReader;
 import java.io.StringWriter;
+import java.net.URL;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
@@ -101,7 +102,12 @@ public class TzServerUtil {
 
   /* ======================= Caching ======================= */
 
-  private static CacheManager manager = new CacheManager();
+  private static CacheManager manager;
+  static {
+    URL u = TzServerUtil.class.getResource("/tzsvrEhcache.xml");
+
+    manager = CacheManager.create(u);
+  }
 
   private static Cache vtzCache;
 
@@ -497,6 +503,7 @@ public class TzServerUtil {
    * ==================================================================== */
 
   static void cacheInit(Properties props) throws ServletException {
+
     vtzCache = manager.getCache(props.getProperty(pnameVtzCache));
   }
 
