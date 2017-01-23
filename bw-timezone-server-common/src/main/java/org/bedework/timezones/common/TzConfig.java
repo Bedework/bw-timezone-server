ccfc5d3ad0a0f5682dc3b46772900e71c0a45c02
mike <mikeadouglass@gmail.com>
2017-01-22 21:38:32 -0500

    Mostly changes to the maven structure. Also remove dependencies on hibernate and ehcache as they are not used

--- bw-timezone-server-common/src/main/java/org/bedework/timezones/common/TzConfig.java
+++ bw-timezone-server-common/src/main/java/org/bedework/timezones/common/TzConfig.java
@@ -21,10 +21,6 @@ package org.bedework.timezones.common;
 import org.bedework.util.config.ConfInfo;
 import org.bedework.util.config.ConfigBase;
 import org.bedework.util.misc.ToString;
-import org.bedework.util.misc.Util;
-
-import java.util.ArrayList;
-import java.util.List;
 
 /** This class defines the various properties we need for a carddav server
  *
@@ -48,8 +44,6 @@ public class TzConfig<T extends TzConfig> extends ConfigBase<T> {
   private String leveldbPath;
 
   private long refreshDelay;
-
-  private List<String> hibernateProperties;
 
   /**
    * @param val the dtstamp
@@ -173,90 +167,6 @@ public class TzConfig<T extends TzConfig> extends ConfigBase<T> {
     return refreshDelay;
   }
 
-  /**
-   *
-   * @param val properties
-   */
-  public void setHibernateProperties(final List<String> val) {
-    hibernateProperties = val;
-  }
-
-  /**
-   *
-   * @return String val
-   */
-  @ConfInfo(collectionElementName = "hibernateProperty")
-  public List<String> getHibernateProperties() {
-    return hibernateProperties;
-  }
-
-  /** Add a hibernate property
-   *
-   * @param name of hibernate property
-   * @param val its value
-   */
-  public void addHibernateProperty(final String name,
-                                   final String val) {
-    List<String> p = getHibernateProperties();
-    if (p == null) {
-      p = new ArrayList<>();
-      setHibernateProperties(p);
-    }
-    p.add(name + "=" + val);
-  }
-
-  /** Get a hibernate property
-   *
-   * @param val name of property
-   * @return value or null
-   */
-  public String getHibernateProperty(final String val) {
-    final List<String> ps = getHibernateProperties();
-
-    final String key = val + "=";
-    for (final String p: ps) {
-      if (p.startsWith(key)) {
-        return p.substring(key.length());
-      }
-    }
-
-    return null;
-  }
-
-  /** Remove a hibernate property
-   *
-   * @param name of property
-   */
-  public void removeHibernateProperty(final String name) {
-    try {
-      final String v = getHibernateProperty(name);
-
-      if (v == null) {
-        return;
-      }
-
-      getHibernateProperties().remove(name + "=" + v);
-    } catch (final Throwable t) {
-      throw new RuntimeException(t);
-    }
-  }
-
-  /** Set a hibernate property
-   *
-   * @param name of property
-   * @param val its value
-   */
-  @SuppressWarnings("UnusedDeclaration")
-  public void setHibernateProperty(final String name,
-                                   final String val) {
-    try {
-      removeHibernateProperty(name);
-      addHibernateProperty(name, val);
-    } catch (final Throwable t) {
-      throw new RuntimeException(t);
-    }
-  }
-
   /** Add our stuff to the StringBuilder
    *
    * @param ts    ToString for result
@@ -277,12 +187,5 @@ public class TzConfig<T extends TzConfig> extends ConfigBase<T> {
     newConf.setPrimaryServer(getPrimaryServer());
     newConf.setTzdataUrl(getTzdataUrl());
     newConf.setRefreshDelay(getRefreshDelay());
-
-    if (!Util.isEmpty(getHibernateProperties())) {
-      for (final String hp: getHibernateProperties()) {
-        final String[] hpels = hp.split("=");
-        newConf.addHibernateProperty(hpels[0], hpels[1]);
-      }
-    }
   }
 }
